d34f0775ed567dd817b1b1db9e5887e90d863830
Jayraj Gajul <jayraj.gajul@mindbowser.com>
2026-05-05 22:49:54 +0530

    feat: integrate redux store and persist into app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import {  RouterProvider } from "react-router-dom"
-import { router } from "./routes/index.tsx"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+
+import { Provider } from "react-redux";
+import { store, persistor } from "@/app/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
   </StrictMode>
-)
+);
