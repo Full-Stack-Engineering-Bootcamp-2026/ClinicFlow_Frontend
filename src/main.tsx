2934959d84d7181aa29df92d711c06b4736ae103
Jayraj Gajul <jayraj.gajul@mindbowser.com>
2026-05-06 11:01:59 +0530

    Added TooltipProvider

--- src/main.tsx
+++ src/main.tsx
@@ -8,26 +8,15 @@ import { router } from "./routes";
 import { Provider } from "react-redux";
 import { store, persistor } from "@/app/store";
 import { PersistGate } from "redux-persist/integration/react";
-
+import { TooltipProvider } from "@/components/ui/tooltip";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+      <TooltipProvider>
       <PersistGate loading={null} persistor={persistor}>
         <RouterProvider router={router} />
       </PersistGate>
+      </TooltipProvider>
     </Provider>
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import { TooltipProvider } from "@/components/ui/tooltip";
-import {  RouterProvider } from "react-router-dom"
-import { router } from "./routes/index.tsx"
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <TooltipProvider>
-          <RouterProvider router={router} />
-
-    </TooltipProvider>
-  </StrictMode>
-);
+    </StrictMode>
+)
